80f33f960b8b33af1321c2c04e6132b7535fec76
Fatman003 <63781742+Fatman003@users.noreply.github.com>
2020-06-22 21:46:11 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 # The directory where the script is
-%env LVSR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+env LVSR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # For PyFST
 if [ -n "$KALDI_ROOT" ];
 then
-    %env LD_LIBRARY_PATH=$KALDI_ROOT/tools/openfst/lib:$LD_LIBRARY_PATH
+    env LD_LIBRARY_PATH=$KALDI_ROOT/tools/openfst/lib:$LD_LIBRARY_PATH
 fi
 
-%env BLOCKS_CONFIG=$LVSR/config/blocks.yaml
-%env THEANORC=$LVSR/config/theano.rc:$HOME/.theanorc
+env BLOCKS_CONFIG=$LVSR/config/blocks.yaml
+env THEANORC=$LVSR/config/theano.rc:$HOME/.theanorc
 
-%env PYTHONPATH=$LVSR:$PYTHONPATH
-%env PATH=$LVSR/bin:$PATH
+env PYTHONPATH=$LVSR:$PYTHONPATH
+env PATH=$LVSR/bin:$PATH
