01dfea3e61a872fcca0b8f54d83c8714755b5fbf
Fatman003 <63781742+Fatman003@users.noreply.github.com>
2020-06-23 14:43:55 +0200

    Update old.sh

--- old.sh
+++ old.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
 # The directory where the script is
-LVSR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
+# export LVSR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+LVSR="/content/Actor"
 # For PyFST
 if [ -n "$KALDI_ROOT" ];
 then
     export LD_LIBRARY_PATH=$KALDI_ROOT/tools/openfst/lib:$LD_LIBRARY_PATH
 fi
 
-export BLOCKS_CONFIG=$LVSR/config/blocks.yaml
-export THEANORC=$LVSR/config/theano.rc:$HOME/.theanorc
+# export BLOCKS_CONFIG=$LVSR/config/blocks.yaml
+BLOCKS_CONFIG="/content/Actor/config/blocks.yaml"
 
-export PYTHONPATH=$LVSR:$PYTHONPATH
-export PATH=$LVSR/bin:$PATH
+#export THEANORC=$LVSR/config/theano.rc:$HOME/.theanorc
+THEANORC="/content/Actor/config/theano.rc:/content/.theanorc"
+
+#export PYTHONPATH=$LVSR:$PYTHONPATH
+import sys
+sys.path.insert(1, "/content/Actor")
+
+#export PATH=$LVSR/bin:$PATH
+import os
+os.environ['PATH'] += ":/content/Actor/bin"
