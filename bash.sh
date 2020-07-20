66302fa714915893978423d5234afdb3a632dcdb
Fatman003 <63781742+Fatman003@users.noreply.github.com>
2020-07-20 17:38:24 +0200

    Add files via upload

---
+++ bash.sh
@@ -0,0 +1,5 @@
+$ pip install toposort pykwalify picklable-itertools pyaml theano==0.9
+$ pip install git+https://github.com/mila-udem/blocks.git@d8b7ffbdda68b4e2ca3c1a2984964285cb1cb709 \-r https://raw.githubusercontent.com/mila-udem/blocks/master/requirements.txt
+$ pip install git+git://github.com/mila-udem/blocks-extras.git@0cefaa3a8a372c551fc3b0df02d5d4f105767d9f
+$ pip install git+git://github.com/mila-udem/fuel.git@42e21a25ed248739e5fe75b9e4193c749979ba57
+$ pip install --upgrade numpy
